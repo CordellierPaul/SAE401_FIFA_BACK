e5e2e679c2c6b6f620bcdc15bdbf6b63b4ad40bd
Anna Kompaniets <anna.kompaniets@etu.univ-smb.fr>
2024-03-11 17:43:45 +0100

    4 tables ( à finir)

---
+++ FIFA_API/Models/EntityFramework/Like_Article.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+
+namespace FIFA_API.Models.EntityFramework
+{
+    [Table("like_article")]
+    public class Like_Article
+    {
+
+        [Key]
+        [Column("idarticle")]
+        public int IdAlbum { get; set; }
+
+        [Key]
+        [Column("adutilisateur")]
+        public int IdUtilisateur { get; set; }
+
+        //rajouter les clés étrangeres
+
+    }
+}
