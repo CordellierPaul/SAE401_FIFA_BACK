461b3bb16d48a9265e7c89d269f06392a56fcfbf
Anna Kompaniets <anna.kompaniets@etu.univ-smb.fr>
2024-03-12 15:13:22 +0100

    fin des tables (partie 4)

---
+++ FIFA_API/Models/EntityFramework/Monnaie.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+
+namespace FIFA_API.Models.EntityFramework
+{
+    [Table("monnaie")]
+    public class Monnaie
+    {
+
+        [Key]
+        [Required]
+        [Column("nummonnaie")]
+        public string NumMonnaie { get; set; }
+
+        [Required]
+        [Column("nommonnaie")]
+        public string NomMonnaie { get; set; }
+
+    }
+}
