8197728b007a13ef1edd28b844f8f5e03036aa51
labauveg <gaelle.labauve@etu.univ-smb.fr>
2024-03-12 15:01:38 +0100

    Création et codage commentaire

---
+++ FIFA_API/Models/EntityFramework/Commentaire.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FIFA_API.Models.EntityFramework
+{
+    [Table("t_e_commentaire_com")]
+    public partial class Commentaire
+    {
+        [Key]
+        [Column("com_id")]
+        public int CommentaireId { get; set; }
+
+        [Column("com_dateheure")]
+        [StringLength(25)]
+        public DateTime CommentaireDateHeure { get; set; } = DateTime.Now;
+
+        [Column("user_id")]
+        public int UtilisateurId { get; set; }
+
+        [Column("com_texte")]
+        [StringLength(1000)]
+        public string CommentaireTexte { get; set; } = null!;
+
+        [Column("com_id")]
+        public int CommentaireIdCommentaire { get; set; }
+
+        [Column("doc_id")]
+        public int DocumentId { get; set; }
+
+        [Column("alb_id")]
+        public int AlbumId { get; set; }
+
+        [Column("blg_id")]
+        public int BlogId { get; set; }
+
+        [Column("art_id")]
+        public int ArticleId { get; set; }
+    }
+}
