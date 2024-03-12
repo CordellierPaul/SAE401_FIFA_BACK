c815d34216283982c00f0a7d0ec594ec3ca39367
CordellierPaul <paul.cordellier@etu.univ-smb.fr>
2024-03-12 15:40:31 +0100

    lien joueur - article + table article et anecdote

---
+++ FIFA_API/Models/EntityFramework/Anecdote.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FIFA_API.Models.EntityFramework
+{
+    [Table("t_e_anecdote_anec")]
+    public class Anecdote
+    {
+        [Key]
+        [Column("anec_id")]
+        public int Id { get; set; }
+
+        // TODO lier à joueur
+        [Column("jou_id")]
+        public int IdJoueur { get; set; }
+
+        [Column("anec_question")]
+        public string Question { get; set; } = null!;
+
+        [Column("anec_reponse")]
+        public string Reponse { get; set; } = null!;
+    }
+}
