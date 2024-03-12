dd3a4ffa8b3823a7d07067d127040021d17946fa
CordellierPaul <paul.cordellier@etu.univ-smb.fr>
2024-03-12 16:05:37 +0100

    ajout blog

---
+++ FIFA_API/Models/EntityFramework/Blog.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FIFA_API.Models.EntityFramework
+{
+    [Table("t_e_blog")]
+    public class Blog
+    {
+        [Key]
+        [Column("blog_id")]
+        public int Id { get; set; }
+
+        // TODO
+    }
+}
--- FIFA_API/Models/EntityFramework/Media.cs
+++ FIFA_API/Models/EntityFramework/Media.cs
@@ -1,8 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-
-
 namespace FIFA_API.Models.EntityFramework
 {
     [Table("t_e_media_med")]
