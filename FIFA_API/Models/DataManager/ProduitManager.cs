ed26638c5a35676731cfe2e09603c6cddf81cc64
labauveg <gaelle.labauve@etu.univ-smb.fr>
2024-03-19 17:20:41 +0100

    Ajout class test Produit sans ode Ajout ProduitManager

---
+++ FIFA_API/Models/DataManager/ProduitManager.cs
@@ -0,0 +1,16 @@
+﻿using FIFA_API.Models.EntityFramework;
+using FIFA_API.Models.Repository;
+
+namespace FIFA_API.Models.DataManager
+{
+    public class ProduitManager : IDataRepository<Produit>
+    {
+        readonly FifaDbContext? fifaDbContext;
+
+        public ProduitManager() { }
+        public ProduitManager(FifaDbContext? context)
+        {
+            fifaDbContext = context;
+        }
+    }
+}
---
+++ FIFA_APITests/Controllers/ProduitControllerTests.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FIFA_API.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FIFA_API.Models.EntityFramework;
+using FIFA_API.Models.Repository;
+using Microsoft.EntityFrameworkCore;
+using FIFA_API.Models.DataManager;
+
+namespace FIFA_API.Controllers.Tests
+{
+    [TestClass()]
+    public class ProduitControllerTests
+    {
+        private FifaDbContext _context;
+        private ProduitController _controller;
+        private IDataRepository<Utilisateur> _dataRepository;
+
+        public ProduitControllerTests()
+        {
+            var builder = new DbContextOptionsBuilder<FifaDbContext>().UseNpgsql("Server=localhost;port=5432;Database=FifaDB; uid=postgres; password=postgres;");
+            _context = new FifaDbContext(builder.Options);
+            //_dataRepository = new ProduitManager(_context);
+            //_controller = new ProduitController(_dataRepository);
+        }
+
+        /*[TestMethod()]
+        public void GetAllProduitTest_Ok()
+        {
+            Assert.Fail();
+        }*/
+
+
+
+        [TestMethod()]
+        public void GetProduitByIdTest_OK()
+        {
+            Produit expected = _context.Produit.Where(u => u.ProduitId == 1).First();
+
+            var result = _controller.GetProduit(1).Result.Value;
+
+            Assert.AreEqual(expected, result, "Pas les mêmes produits");
+        }
+    }
+}
