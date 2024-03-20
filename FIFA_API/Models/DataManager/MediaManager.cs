b2da94d79a0f93f76d28775fb41ee2aff8ccf198
Anna Kompaniets <anna.kompaniets@etu.univ-smb.fr>
2024-03-20 16:17:40 +0100

    MediaManager fait

---
+++ FIFA_API/Models/DataManager/MediaManager.cs
@@ -0,0 +1,57 @@
+﻿using FIFA_API.Models.EntityFramework;
+using FIFA_API.Models.Repository;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace FIFA_API.Models.DataManager
+{
+    public class MediaManager : IDataRepository<Media>
+    {
+        private readonly FifaDbContext fifaDbContext;
+
+        public MediaManager(FifaDbContext context)
+        {
+            fifaDbContext = context;
+        }
+
+        public async Task<ActionResult<IEnumerable<Media>>> GetAllAsync()
+        {
+            return await fifaDbContext.Media.ToListAsync();
+        }
+
+        public async Task AddAsync(Media entity)
+        {
+            await fifaDbContext.Media.AddAsync(entity);
+            await fifaDbContext.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(Media entity)
+        {
+            fifaDbContext.Media.Remove(entity);
+            await fifaDbContext.SaveChangesAsync();
+        }
+
+
+        public async Task<ActionResult<Media>> GetByIdAsync(int id)
+        {
+            return await fifaDbContext.Media.FirstOrDefaultAsync(u => u.MediaId == id);
+
+        }
+
+        public async Task<ActionResult<Media>> GetByStringAsync(string str)
+        {
+            throw new NotImplementedException(); // a changer le répository
+        }
+
+        public async Task UpdateAsync(Media entityToUpdate, Media entity)
+        {
+            fifaDbContext.Entry(entityToUpdate).State = EntityState.Modified;
+            entityToUpdate.MediaId = entity.MediaId;
+            entityToUpdate.MediaUrl = entity.MediaUrl;
+            entityToUpdate.LiensArticles = entity.LiensArticles;
+            entityToUpdate.MediaFilm = entity.MediaFilm;
+            entityToUpdate.ImagesMedia = entity.ImagesMedia;
+            await fifaDbContext.SaveChangesAsync();
+        }
+    }
+}
