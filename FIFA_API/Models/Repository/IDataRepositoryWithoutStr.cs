2a6574c2f6c9f1f4b25b81641f2cd3e1f169e723
Anna Kompaniets <anna.kompaniets@etu.univ-smb.fr>
2024-03-20 17:09:13 +0100

    Nouvelle interface IDataRepositoryWithoutString pour les tables sans recherche par string

---
+++ FIFA_API/Models/Repository/IDataRepositoryWithoutStr.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FIFA_API.Models.Repository
+{
+    public interface IDataRepositoryWithoutStr<TEntity>
+    {
+        Task<ActionResult<IEnumerable<TEntity>>> GetAllAsync();
+        Task<ActionResult<TEntity>> GetByIdAsync(int id);
+        Task AddAsync(TEntity entity);
+        Task UpdateAsync(TEntity entityToUpdate, TEntity entity);
+        Task DeleteAsync(TEntity entity);
+    }
+
+}
+}
