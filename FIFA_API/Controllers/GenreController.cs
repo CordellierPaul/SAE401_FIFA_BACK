c2a36ee9e7c17f807e64c4ecec88e6e188ea2fab
labauveg <gaelle.labauve@etu.univ-smb.fr>
2024-03-20 17:10:56 +0100

    Codage repostory GenreController

--- FIFA_API/Controllers/GenreController.cs
+++ FIFA_API/Controllers/GenreController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using FIFA_API.Models.EntityFramework;
+using FIFA_API.Models.Repository;
 
 namespace FIFA_API.Controllers
 {
@@ -13,111 +14,94 @@ namespace FIFA_API.Controllers
     [ApiController]
     public class GenreController : ControllerBase
     {
-        private readonly FifaDbContext _context;
+        private readonly IDataRepository<Genre> dataRepository;
 
-        public GenreController(FifaDbContext context)
+        public GenreController(IDataRepository<Genre> context)
         {
-            _context = context;
+            dataRepository = context;
         }
 
         // GET: api/Genre
         [HttpGet]
         public async Task<ActionResult<IEnumerable<Genre>>> GetGenre()
         {
-          if (_context.Genre == null)
-          {
-              return NotFound();
-          }
-            return await _context.Genre.ToListAsync();
+            return await dataRepository.GetAllAsync();
         }
 
         // GET: api/Genre/5
-        [HttpGet("{id}")]
-        public async Task<ActionResult<Genre>> GetGenre(int id)
+        [HttpGet]
+        [Route("[action]/{id}")]
+        [ActionName("GetById")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<Genre>> GetGenreById(int id)
         {
-          if (_context.Genre == null)
-          {
-              return NotFound();
-          }
-            var genre = await _context.Genre.FindAsync(id);
+            var genre = await dataRepository.GetByIdAsync(id);
 
             if (genre == null)
             {
                 return NotFound();
             }
-
             return genre;
         }
 
         // PUT: api/Genre/5
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPut("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
         public async Task<IActionResult> PutGenre(int id, Genre genre)
         {
             if (id != genre.GenreId)
             {
                 return BadRequest();
             }
-
-            _context.Entry(genre).State = EntityState.Modified;
-
-            try
+            var genToUpdate = await dataRepository.GetByIdAsync(id);
+            if (genToUpdate == null)
             {
-                await _context.SaveChangesAsync();
+                return NotFound();
             }
-            catch (DbUpdateConcurrencyException)
+            else
             {
-                if (!GenreExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
+                await dataRepository.UpdateAsync(genToUpdate.Value, genre);
+                return NoContent();
             }
-
-            return NoContent();
         }
 
         // POST: api/Genre
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
         public async Task<ActionResult<Genre>> PostGenre(Genre genre)
         {
-          if (_context.Genre == null)
-          {
-              return Problem("Entity set 'FifaDbContext.Genre'  is null.");
-          }
-            _context.Genre.Add(genre);
-            await _context.SaveChangesAsync();
-
-            return CreatedAtAction("GetGenre", new { id = genre.GenreId }, genre);
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            await dataRepository.AddAsync(genre);
+            return CreatedAtAction("GetById", new { id = genre.GenreId }, genre);
         }
 
         // DELETE: api/Genre/5
         [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public async Task<IActionResult> DeleteGenre(int id)
         {
-            if (_context.Genre == null)
-            {
-                return NotFound();
-            }
-            var genre = await _context.Genre.FindAsync(id);
+            var genre = await dataRepository.GetByIdAsync(id);
             if (genre == null)
             {
                 return NotFound();
             }
-
-            _context.Genre.Remove(genre);
-            await _context.SaveChangesAsync();
-
+            await dataRepository.DeleteAsync(genre.Value);
             return NoContent();
         }
 
-        private bool GenreExists(int id)
+        /*private bool GenreExists(int id)
         {
-            return (_context.Genre?.Any(e => e.GenreId == id)).GetValueOrDefault();
-        }
+            return (dataRepository.Genre?.Any(e => e.GenreId == id)).GetValueOrDefault();
+        }*/
     }
 }
