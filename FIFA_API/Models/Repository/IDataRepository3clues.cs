5af656337f24a14ca63dee19e6bf91c0249f56bd
Anna Kompaniets <anna.kompaniets@etu.univ-smb.fr>
2024-03-20 17:23:16 +0100

    rajout 2 nouvelles interfaces : pour les jointures avec 2 et 3 clés

---
+++ FIFA_API/Models/Repository/IDataRepository3clues.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FIFA_API.Models.Repository
+{
+    public interface IDataRepository3clues<TEntity>
+    {
+        Task<ActionResult<IEnumerable<TEntity>>> GetAllAsync();
+        Task<ActionResult<TEntity>> GetByIdAsync(int id1, int id2, int id3);
+        Task<ActionResult<TEntity>> GetByStringAsync(string str);
+        Task AddAsync(TEntity entity);
+        Task UpdateAsync(TEntity entityToUpdate, TEntity entity);
+        Task DeleteAsync(TEntity entity);
+    }
+}
+
