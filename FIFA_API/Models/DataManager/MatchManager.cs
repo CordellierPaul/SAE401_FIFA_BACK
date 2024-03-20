ae74d935600a8e52afbcb94a7dd2170c733c252d
Anna Kompaniets <anna.kompaniets@etu.univ-smb.fr>
2024-03-20 16:13:13 +0100

    MatchManager fait

---
+++ FIFA_API/Models/DataManager/MatchManager.cs
@@ -0,0 +1,60 @@
+﻿using FIFA_API.Models.EntityFramework;
+using FIFA_API.Models.Repository;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace FIFA_API.Models.DataManager
+{
+    public class MatchManager : IDataRepository<Match>
+    {
+        private readonly FifaDbContext fifaDbContext;
+
+        public MatchManager(FifaDbContext context)
+        {
+            fifaDbContext = context;
+        }
+
+        public async Task<ActionResult<IEnumerable<Match>>> GetAllAsync()
+        {
+            return await fifaDbContext.Match.ToListAsync();
+        }
+
+        public async Task AddAsync(Match entity)
+        {
+            await fifaDbContext.Match.AddAsync(entity);
+            await fifaDbContext.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(Match entity)
+        {
+            fifaDbContext.Match.Remove(entity);
+            await fifaDbContext.SaveChangesAsync();
+        }
+
+
+        public async Task<ActionResult<Match>> GetByIdAsync(int id)
+        {
+            return await fifaDbContext.Match.FirstOrDefaultAsync(u => u.MatchId == id);
+
+        }
+
+        public async Task<ActionResult<Match>> GetByStringAsync(string str)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task UpdateAsync(Match entityToUpdate, Match entity)
+        {
+            fifaDbContext.Entry(entityToUpdate).State = EntityState.Modified;
+            entityToUpdate.MatchId = entity.MatchId;
+            entityToUpdate.ClubDomicileId = entity.ClubDomicileId;
+            entityToUpdate.ClubExterieurId = entity.ClubExterieurId;
+            entityToUpdate.MatchDate = entity.MatchDate;
+            entityToUpdate.MatchScoreDomicile = entity.MatchScoreDomicile;
+            entityToUpdate.MatchScoreExterieur = entity.MatchScoreExterieur;
+            entityToUpdate.Matches_joue = entity.Matches_joue;
+            await fifaDbContext.SaveChangesAsync();
+        }
+    }
+}
