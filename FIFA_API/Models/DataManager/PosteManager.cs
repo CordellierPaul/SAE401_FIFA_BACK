82251bfe0702ef90fd59dcc97ec4a27fb22669f5
Anna Kompaniets <anna.kompaniets@etu.univ-smb.fr>
2024-03-20 16:28:09 +0100

    PosteManager fait + theme -> e et non pas j

---
+++ FIFA_API/Models/DataManager/PosteManager.cs
@@ -0,0 +1,55 @@
+﻿using FIFA_API.Models.EntityFramework;
+using FIFA_API.Models.Repository;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace FIFA_API.Models.DataManager
+{
+    public class PosteManager : IDataRepository<Poste>
+    {
+        private readonly FifaDbContext fifaDbContext;
+
+        public PosteManager(FifaDbContext context)
+        { 
+            fifaDbContext = context;
+        }
+
+        public async Task<ActionResult<IEnumerable<Poste>>> GetAllAsync()
+        {
+            return await fifaDbContext.Poste.ToListAsync();
+        }
+
+        public async Task AddAsync(Poste entity)
+        {
+            await fifaDbContext.Poste.AddAsync(entity);
+            await fifaDbContext.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(Poste entity)
+        {
+            fifaDbContext.Poste.Remove(entity);
+            await fifaDbContext.SaveChangesAsync();
+        }
+
+
+        public async Task<ActionResult<Poste>> GetByIdAsync(int id)
+        {
+            return await fifaDbContext.Poste.FirstOrDefaultAsync(u => u.PosteId == id);
+
+        }
+
+        public async Task<ActionResult<Poste>> GetByStringAsync(string str)
+        {
+            return await fifaDbContext.Poste.FirstOrDefaultAsync(u => u.PosteLibelle.ToUpper() == str.ToUpper());
+        }
+
+        public async Task UpdateAsync(Poste entityToUpdate, Poste entity)
+        {
+            fifaDbContext.Entry(entityToUpdate).State = EntityState.Modified;
+            entityToUpdate.PosteId = entity.PosteId;
+            entityToUpdate.PosteLibelle = entity.PosteLibelle;
+            entityToUpdate.JoueursPoste = entity.JoueursPoste;
+            await fifaDbContext.SaveChangesAsync();
+        }
+    }
+}
