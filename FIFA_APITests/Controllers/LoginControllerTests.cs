192411f555726f128e2f5c9dd1d1e45c0804179e
CordellierPaul <paul.cordellier@etu.univ-smb.fr>
2024-03-21 17:23:53 +0100

    début test

---
+++ FIFA_APITests/Controllers/LoginControllerTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace FIFA_APITests.Controllers
+{
+    [TestClass()]
+    public class LoginControllerTests
+    {
+
+        [TestMethod()]
+        public void LoginControllerTest_200OkResult()
+        {
+
+        }
+
+        [TestMethod()]
+        public void LoginControllerTest_401Unauthorized()
+        {
+
+        }
+    }
+}
