79dc98478692d8b8bf3ebed4c093fa70b1e85a08
Anna Kompaniets <anna.kompaniets@etu.univ-smb.fr>
2024-03-21 11:32:35 +0100

    LikeAkbumController amelioré (à modifier car 2 clés)

--- FIFA_API/Controllers/LikeAlbumController.cs
+++ FIFA_API/Controllers/LikeAlbumController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using FIFA_API.Models.EntityFramework;
+using FIFA_API.Models.Repository;
 
 namespace FIFA_API.Controllers
 {
@@ -13,125 +14,95 @@ namespace FIFA_API.Controllers
     [ApiController]
     public class LikeAlbumController : ControllerBase
     {
-        private readonly FifaDbContext _context;
+        private readonly IDataRepository<LikeAlbum> dataRepository;
 
-        public LikeAlbumController(FifaDbContext context)
+        public LikeAlbumController(IDataRepository<LikeAlbum> context)
         {
-            _context = context;
+            dataRepository = context;
         }
 
         // GET: api/LikeAlbum
         [HttpGet]
         public async Task<ActionResult<IEnumerable<LikeAlbum>>> GetLikeAlbum()
         {
-          if (_context.LikeAlbum == null)
-          {
-              return NotFound();
-          }
-            return await _context.LikeAlbum.ToListAsync();
+            return await dataRepository.GetAllAsync();
         }
 
         // GET: api/LikeAlbum/5
-        [HttpGet("{id}")]
-        public async Task<ActionResult<LikeAlbum>> GetLikeAlbum(int id)
+        [HttpGet]
+        [Route("[action]/{id}")]
+        [ActionName("GetById")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<LikeAlbum>> GetLikeAlbumById(int id)
         {
-          if (_context.LikeAlbum == null)
-          {
-              return NotFound();
-          }
-            var likeAlbum = await _context.LikeAlbum.FindAsync(id);
+            var album = await dataRepository.GetByIdAsync(id);
 
-            if (likeAlbum == null)
+            if (album == null)
             {
                 return NotFound();
             }
 
-            return likeAlbum;
+            return album;
         }
 
         // PUT: api/LikeAlbum/5
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPut("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
         public async Task<IActionResult> PutLikeAlbum(int id, LikeAlbum likeAlbum)
         {
             if (id != likeAlbum.AlbumId)
             {
                 return BadRequest();
             }
-
-            _context.Entry(likeAlbum).State = EntityState.Modified;
-
-            try
+            var albToUpdate = await dataRepository.GetByIdAsync(id);
+            if (albToUpdate == null)
             {
-                await _context.SaveChangesAsync();
+                return NotFound();
             }
-            catch (DbUpdateConcurrencyException)
+            else
             {
-                if (!LikeAlbumExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
+                await dataRepository.UpdateAsync(albToUpdate.Value, likeAlbum);
+                return NoContent();
             }
-
-            return NoContent();
         }
 
         // POST: api/LikeAlbum
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
         public async Task<ActionResult<LikeAlbum>> PostLikeAlbum(LikeAlbum likeAlbum)
         {
-          if (_context.LikeAlbum == null)
-          {
-              return Problem("Entity set 'FifaDbContext.LikeAlbum'  is null.");
-          }
-            _context.LikeAlbum.Add(likeAlbum);
-            try
+            if (!ModelState.IsValid)
             {
-                await _context.SaveChangesAsync();
+                return BadRequest(ModelState);
             }
-            catch (DbUpdateException)
-            {
-                if (LikeAlbumExists(likeAlbum.AlbumId))
-                {
-                    return Conflict();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-
-            return CreatedAtAction("GetLikeAlbum", new { id = likeAlbum.AlbumId }, likeAlbum);
+            await dataRepository.AddAsync(likeAlbum);
+            return CreatedAtAction("GetById", new { id = likeAlbum.AlbumId }, likeAlbum);
         }
 
         // DELETE: api/LikeAlbum/5
         [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public async Task<IActionResult> DeleteLikeAlbum(int id)
         {
-            if (_context.LikeAlbum == null)
-            {
-                return NotFound();
-            }
-            var likeAlbum = await _context.LikeAlbum.FindAsync(id);
+            var likeAlbum = await dataRepository.GetByIdAsync(id);
             if (likeAlbum == null)
             {
                 return NotFound();
             }
-
-            _context.LikeAlbum.Remove(likeAlbum);
-            await _context.SaveChangesAsync();
-
+            await dataRepository.DeleteAsync(likeAlbum.Value);
             return NoContent();
         }
 
-        private bool LikeAlbumExists(int id)
-        {
-            return (_context.LikeAlbum?.Any(e => e.AlbumId == id)).GetValueOrDefault();
-        }
+        //private bool LikeAlbumExists(int id)
+        //{
+        //    return (dataRepository.LikeAlbum?.Any(e => e.AlbumId == id)).GetValueOrDefault();
+        //}
     }
 }
