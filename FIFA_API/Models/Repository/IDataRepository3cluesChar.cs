6f342fd3819fb1b014ab43c798dc1cbd1f2d3e47
Anna Kompaniets <anna.kompaniets@etu.univ-smb.fr>
2024-03-21 10:29:35 +0100

    Remportemanager fait + une nouvelle IDR car besoin 3 clés dont un char et pas int

---
+++ FIFA_API/Models/Repository/IDataRepository3cluesChar.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FIFA_API.Models.Repository
+{
+    public interface IDataRepository3cluesChar<TEntity>
+    {
+        Task<ActionResult<IEnumerable<TEntity>>> GetAllAsync();
+        Task<ActionResult<TEntity>> GetByIdAsync(int id1, int id2, char id3);
+        Task AddAsync(TEntity entity);
+        Task UpdateAsync(TEntity entityToUpdate, TEntity entity);
+        Task DeleteAsync(TEntity entity);
+    }
+}
+
