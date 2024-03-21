6a7fbb241d159df132f26ebed361e852f7bf2730
Akinaru <maxime.utchimata@gmail.com>
2024-03-21 16:42:05 +0100

    filtres

--- FIFA_CLIENT/src/pages/produits.vue
+++ FIFA_CLIENT/src/pages/produits.vue
@@ -1,21 +1,17 @@
 <script setup>
 import ProduitComponent from '@/components/ProduitComponent.vue'
-
+import FiltreComponent from '@/components/FiltreComponent.vue';
 </script>
 
 <template>
     <div class="flex">
         <div id="left_part" class="bg-base-300 hidden lg:block w-72">
             <p class="flex justify-center text-xl">Filtres</p>
-            <div class="divider"></div> 
-            <div class="bg-base-100">
-                <div class="ml-5">
-                    <p class="text-lg">Pays</p>
-                    <ul class="">
-                        <div class="flex items-center"><input type="checkbox" class="checkbox checkbox-sm"/><span class="ml-4">France</span></div>
-                    </ul>
-                </div>
-            </div>
+            
+            <FiltreComponent :filtreData="{ titre: 'Taille', options: ['S', 'M', 'L', 'XL'] }" />
+            <FiltreComponent :filtreData="{ titre: 'Genre', options: ['Homme', 'Femme', 'Jeune'] }" />
+
+
         </div>
         <div id="right_part" class="w-full">
             <div class="bg-base-200 p-10">
