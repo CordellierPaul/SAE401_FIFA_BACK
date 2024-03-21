3d45b619df24e67c7fd937d19c1f0a4cad2f4d9d
Anna Kompaniets <anna.kompaniets@etu.univ-smb.fr>
2024-03-21 09:25:16 +0100

    ImageJoueurmanager fait

---
+++ FIFA_API/Models/DataManager/ImageJoueurManager.cs
@@ -0,0 +1,50 @@
+﻿using FIFA_API.Models.EntityFramework;
+using FIFA_API.Models.Repository;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace FIFA_API.Models.DataManager
+{
+    public class ImageJoueurManager : IDataRepository2clues<ImageJoueur>
+    {
+
+        readonly FifaDbContext? fifaDbContext;
+
+        public ImageJoueurManager(FifaDbContext context)
+        {
+            fifaDbContext = context;
+        }
+        public async Task<ActionResult<IEnumerable<ImageJoueur>>> GetAllAsync()
+        {
+            return await fifaDbContext.ImageJoueur.ToListAsync();
+        }
+
+        public async Task AddAsync(ImageJoueur entity)
+        {
+            await fifaDbContext.ImageJoueur.AddAsync(entity);
+            await fifaDbContext.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(ImageJoueur entity)
+        {
+            fifaDbContext.ImageJoueur.Remove(entity);
+            await fifaDbContext.SaveChangesAsync();
+        }
+
+
+        public async Task<ActionResult<ImageJoueur>> GetByIdAsync(int uid, int tid)
+        {
+            return await fifaDbContext.ImageJoueur.FirstOrDefaultAsync(u => u.ImageId == uid && u.JoueurId == tid);
+        }
+
+
+        public async Task UpdateAsync(ImageJoueur entityToUpdate, ImageJoueur entity)
+        {
+            fifaDbContext.Entry(entityToUpdate).State = EntityState.Modified;
+            entityToUpdate.JoueurId = entity.JoueurId;
+            entityToUpdate.ImageId = entity.ImageId;
+            await fifaDbContext.SaveChangesAsync();
+        }
+    }
+}
