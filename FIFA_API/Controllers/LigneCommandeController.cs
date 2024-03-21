88e296a0d081f310ee0368ae156cd1b0677c7a39
Anna Kompaniets <anna.kompaniets@etu.univ-smb.fr>
2024-03-21 11:02:29 +0100

    LigneCommandeController amelioré

--- FIFA_API/Controllers/LigneCommandeController.cs
+++ FIFA_API/Controllers/LigneCommandeController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using FIFA_API.Models.EntityFramework;
+using FIFA_API.Models.Repository;
 
 namespace FIFA_API.Controllers
 {
@@ -13,111 +14,95 @@ namespace FIFA_API.Controllers
     [ApiController]
     public class LigneCommandeController : ControllerBase
     {
-        private readonly FifaDbContext _context;
+        private readonly IDataRepository<LigneCommande> dataRepository;
 
-        public LigneCommandeController(FifaDbContext context)
+        public LigneCommandeController(IDataRepository<LigneCommande> context)
         {
-            _context = context;
+            dataRepository = context;
         }
 
         // GET: api/LigneCommande
         [HttpGet]
         public async Task<ActionResult<IEnumerable<LigneCommande>>> GetLigneCommande()
         {
-          if (_context.LigneCommande == null)
-          {
-              return NotFound();
-          }
-            return await _context.LigneCommande.ToListAsync();
+            return await dataRepository.GetAllAsync();
         }
 
         // GET: api/LigneCommande/5
-        [HttpGet("{id}")]
-        public async Task<ActionResult<LigneCommande>> GetLigneCommande(int id)
+        [HttpGet]
+        [Route("[action]/{id}")]
+        [ActionName("GetById")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<LigneCommande>> GetLigneCommandeById(int id)
         {
-          if (_context.LigneCommande == null)
-          {
-              return NotFound();
-          }
-            var ligneCommande = await _context.LigneCommande.FindAsync(id);
+            var lignecommande = await dataRepository.GetByIdAsync(id);
 
-            if (ligneCommande == null)
+            if (lignecommande == null)
             {
                 return NotFound();
             }
 
-            return ligneCommande;
+            return lignecommande;
         }
 
         // PUT: api/LigneCommande/5
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPut("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
         public async Task<IActionResult> PutLigneCommande(int id, LigneCommande ligneCommande)
         {
             if (id != ligneCommande.LigneCommandeId)
             {
                 return BadRequest();
             }
-
-            _context.Entry(ligneCommande).State = EntityState.Modified;
-
-            try
+            var lcdToUpdate = await dataRepository.GetByIdAsync(id);
+            if (lcdToUpdate == null)
             {
-                await _context.SaveChangesAsync();
+                return NotFound();
             }
-            catch (DbUpdateConcurrencyException)
+            else
             {
-                if (!LigneCommandeExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
+                await dataRepository.UpdateAsync(lcdToUpdate.Value, ligneCommande);
+                return NoContent();
             }
-
-            return NoContent();
         }
 
         // POST: api/LigneCommande
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
         public async Task<ActionResult<LigneCommande>> PostLigneCommande(LigneCommande ligneCommande)
         {
-          if (_context.LigneCommande == null)
-          {
-              return Problem("Entity set 'FifaDbContext.LigneCommande'  is null.");
-          }
-            _context.LigneCommande.Add(ligneCommande);
-            await _context.SaveChangesAsync();
-
-            return CreatedAtAction("GetLigneCommande", new { id = ligneCommande.LigneCommandeId }, ligneCommande);
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            await dataRepository.AddAsync(ligneCommande);
+            return CreatedAtAction("GetById", new { id = ligneCommande.LigneCommandeId }, ligneCommande);
         }
 
         // DELETE: api/LigneCommande/5
         [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public async Task<IActionResult> DeleteLigneCommande(int id)
         {
-            if (_context.LigneCommande == null)
-            {
-                return NotFound();
-            }
-            var ligneCommande = await _context.LigneCommande.FindAsync(id);
+            var ligneCommande = await dataRepository.GetByIdAsync(id);
             if (ligneCommande == null)
             {
                 return NotFound();
             }
-
-            _context.LigneCommande.Remove(ligneCommande);
-            await _context.SaveChangesAsync();
-
+            await dataRepository.DeleteAsync(ligneCommande.Value);
             return NoContent();
         }
 
         private bool LigneCommandeExists(int id)
         {
-            return (_context.LigneCommande?.Any(e => e.LigneCommandeId == id)).GetValueOrDefault();
+            return (dataRepository.LigneCommande?.Any(e => e.LigneCommandeId == id)).GetValueOrDefault();
         }
     }
 }
