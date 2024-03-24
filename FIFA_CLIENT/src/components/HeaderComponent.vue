049df0b617ca0d0aed02cdc724796cbb711435c9
Akinaru <maxime.utchimata@gmail.com>
2024-03-24 16:42:13 +0100

    header

--- FIFA_CLIENT/src/components/HeaderComponent.vue
+++ FIFA_CLIENT/src/components/HeaderComponent.vue
@@ -50,13 +50,13 @@
           <div tabindex="0" role="button" class="btn btn-primary hover:opacity-70 btn-circle">
             <div class="indicator">
               <svg xmlns="http://www.w3.org/2000/svg" class="h-7 w-7" fill="none" viewBox="0 0 24 24" stroke="white"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" /></svg>
-              <span class="badge badge-sm badge-accent indicator-item">8</span>
+              <span class="badge badge-sm badge-accent indicator-item">1</span>
             </div>
           </div>
           <div tabindex="0" class="mt-3 z-[1] card card-compact dropdown-content w-52 bg-base-200 shadow">
             <div class="card-body">
-              <span class="font-bold text-lg">0 Articles</span>
-              <span class="text-info">Total: 0,00€</span>
+              <span class="font-bold text-lg">1 Articles</span>
+              <span class="text-info">Total: 1749,00€</span>
               <div class="card-actions">
                 <RouterLink :to="{name: 'panier'}"><button class="btn btn-primary btn-block">Voir le panier</button></RouterLink>
                 
