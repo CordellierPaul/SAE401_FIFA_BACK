cab3935bcd8d364fe22b868d3a9962c039a35500
evti <obit2004@hotmail.com>
2024-03-24 18:12:02 +0100

    flowbite

--- FIFA_CLIENT/src/pages/produit.vue
+++ FIFA_CLIENT/src/pages/produit.vue
@@ -8,14 +8,14 @@
                 <li><RouterLink :to="{name: 'index'}" class="hover:opacity-50 hover:cursor-pointer">FIFA</RouterLink></li> 
                 <li><a @click= "retour"  class="hover:opacity-50 hover:cursor-pointer">Produits</a></li> 
                 <!-- Titre de la page actuelle -->
-                <li>ADIDAS ARGENTINA WINNERS HOME JERSEY - WOMEN'S</li>
+                <li>MAILLOT DOMICILE VAINQUEUR ARGENTINE ADIDAS – FEMME</li>
             </ul>
         </div>
     </div>
 
 
-    <div class="flex">
-        <div  class="bg-base-300 hidden lg:block w-1/2 p-2">
+    <div class="flex bg-base-200">
+        <div  class=" hidden lg:block w-1/2 p-2">
             <div class="carousel carousel-center size-11/12 p-8 space-x-4 bg-neutral rounded-box scrollbar" >
                 <div class="carousel-item w-fit">
                     <img src="https://store.fifa.com/_next/image?url=https%3A%2F%2Flegends.broadleafcloud.com%2Fapi%2Fasset%2Fcontent%2FARG%2520FRONT.png%3FcontextRequest%3D%257B%2522forceCatalogForFetch%2522%3Afalse%2C%2522applicationId%2522%3A%252201GPYEXET5B7Y61HW8TB4R0YWE%2522%2C%2522tenantId%2522%3A%2522FIFA%2522%257D&w=3840&q=85" class="rounded-box" alt="Tailwind CSS Carousel component" />
@@ -38,9 +38,10 @@
             </div>
         </div>
 
-    
-        <div  class="w-1/2 bg-base-300 p-2">
-            <p class="text-2xl font-bold">ADIDAS ARGENTINA WINNERS HOME JERSEY - WOMEN'S</p>
+        
+
+        <div  class="w-1/2  p-2">
+            <p class="text-2xl font-bold">MAILLOT DOMICILE VAINQUEUR ARGENTINE ADIDAS – FEMME</p>
             <p class="text-2xl">90€</p>
             <div class="flex justify-between my-3">
                 <p>Taille</p>
@@ -65,15 +66,22 @@
                 <p class="font-semibold">Description</p>
                 <p class="my-5">Un style indémodable pour les vainqueurs de la Coupe du Monde de la FIFA 2022™. Que les puristes se rassurent, on retrouve bien évidemment les mythiques bandes bleues et blanches, ainsi que le fameux emblème « Sol de Mayo », sur ce maillot domicile de l’Argentine. Les trois étoiles évoquent les titres mondiaux de l’Albiceleste, dont le sacre décroché au Qatar. Doté de la technologie absorbante AEROREADY et d’un écusson brodé, ce maillot permet d’exprimer votre passion dans un confort optimal en toute circonstance. Cet article est fabriqué à l’aide de matériaux 100% recyclés. Réutiliser d’anciens articles nous permet de diminuer les déchets, notre dépendance aux ressources limitées ainsi que l’empreinte carbone de nos produits.</p>
             </div>
-        </div>
-
+        </div>    
     </div>
-
+    
+    <div class="p-2 w-full">
+        <p class="text-2xl font-bold">Produits associés</p>
+        <div id="container" class="flex w-full bg-red-50 gap-10 p-2">
+            <ProduitComponent v-for="index in 21" :key="index" />
+        </div>
+    </div>
+    
 </template>
     
 <script setup>
     import { defineProps } from 'vue';
     import { useRoute,useRouter } from 'vue-router';
+    import ProduitComponent from '@/components/ProduitComponent.vue';
 
     const router = useRouter();
 
--- FIFA_CLIENT/tailwind.config.js
+++ FIFA_CLIENT/tailwind.config.js
@@ -2,13 +2,19 @@
 module.exports = {
   purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
+  content: [
+    "./node_modules/flowbite/**/*.js"
+  ],
   theme: {
     extend: {},
   },
   variants: {
     extend: {},
   },
-  plugins: [require("daisyui")],
+  plugins: [
+    require('daisyui'),
+    require('flowbite/plugin')
+  ],
   daisyui: {
     themes: [
       {
