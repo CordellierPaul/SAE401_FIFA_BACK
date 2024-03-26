e01f4061ba9ece9b3e7c39d68b182b46380d6559
labauveg <gaelle.labauve@etu.univ-smb.fr>
2024-03-26 11:39:31 +0100

    Ajout services pour Livraison

--- FIFA_API/Program.cs
+++ FIFA_API/Program.cs
@@ -49,6 +49,7 @@ builder.Services.AddScoped<IDataRepository<InfosBancaires>, InfosBancairesManage
 builder.Services.AddScoped<IDataRepository<Joueur>, JoueurManager>();
 builder.Services.AddScoped<IDataRepository<Langue>, LangueManager>();
 builder.Services.AddScoped<IDataRepositoryWithoutStr<LigneCommande>, LigneCommandeManager>();
+builder.Services.AddScoped<IDataRepository<Livraison>, LivraisonManager>();
 builder.Services.AddScoped<IDataRepositoryWithoutStr<Match>, MatchManager>();
 builder.Services.AddScoped<IDataRepositoryWithoutStr<Media>, MediaManager>();
 builder.Services.AddScoped<IDataRepository<Monnaie>, MonnaieManager>();
