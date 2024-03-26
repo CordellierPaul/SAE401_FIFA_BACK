d7c11deea0965b0494c8a4006e92066dc5353774
Anna Kompaniets <anna.kompaniets@etu.univ-smb.fr>
2024-03-26 17:42:44 +0100

    theme.vue pas tres fonctionnel (proxy...)

--- FIFA_CLIENT/src/pages/theme.vue
+++ FIFA_CLIENT/src/pages/theme.vue
@@ -1,9 +1,42 @@
+
 <script setup>
+    import { ref, onMounted } from 'vue';
+
+    const themes = ref([]);
+
+    async function fetchThemes() {
+        try {
+        const response = await fetch('https://apififa.azurewebsites.net/api/Theme', {
+            method: 'GET',
+            mode: 'cors'
+        });
+
+        const data = await response.json();
+
+        themes.value = data;
+
+        console.log(themes.value, "hehe");
+        //proxy??
+        } catch (error) {
+        console.error('Erreur lors de la récupération des thèmes :', error);
+        }
+    }
+
+    onMounted(fetchThemes);
 </script>
 
-<template> 
+<template>
+    <div>
+      <h1>Liste des thèmes</h1>
+        <br>
+      <ul>
+        <li v-for="theme in themes" :key="theme.themeId">
+          {{ theme.themeLibelle }}
+          3
+        </li>
+      </ul>
+      <br>
+    </div>
+  </template>
+  
 
-</template>
-
-<style scoped> 
-</style>
