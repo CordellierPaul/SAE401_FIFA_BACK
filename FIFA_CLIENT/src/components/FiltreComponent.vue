47ca3003fbdc23b0dbf8988b1ed0a07fa776bd73
evti <obit2004@hotmail.com>
2024-03-27 12:22:45 +0100

    recuperation filtre checked

--- FIFA_CLIENT/src/components/FiltreComponent.vue
+++ FIFA_CLIENT/src/components/FiltreComponent.vue
@@ -5,7 +5,7 @@
       <div class="ml-5">
         <p class="text-lg mb-4">{{ titre }}</p>
         <div v-for="option in options" :key="option" class="flex items-center">
-          <input type="checkbox" class="checkbox checkbox-accent checkbox-sm "/>
+          <input @click="toggleOption(option)" type="checkbox" class="checkbox checkbox-accent checkbox-sm "/>
           <span class="ml-4">{{ option }}</span>
         </div>
       </div>
@@ -13,7 +13,7 @@
 </template>
   
   <script setup>
-  import { defineProps } from 'vue';
+  import { defineProps, ref } from 'vue';
   
   const props = defineProps({
     filtreData: {
@@ -25,7 +25,26 @@
   const titre = props.filtreData.titre;
   const options = props.filtreData.options;
 
+  const optionsCheck = []
+
+  function toggleOption(option){
+
+    if (optionsCheck.includes(option)) {
+      optionsCheck.splice(optionsCheck.indexOf(option),1)
+    }
+    else{
+      optionsCheck.push(option) 
+    }
+
+    console.log(optionsCheck);
+
+  }
   
+  const emit = defineEmits(['next','previous'])
+
+  function btPreviousClick() {
+      emit('previous')
+  }
 
   </script>
   
