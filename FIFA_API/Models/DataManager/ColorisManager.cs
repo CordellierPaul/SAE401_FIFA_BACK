4cbaa4860352649c9a3cec065db1aaa8e3b05f9b
Anna Kompaniets <anna.kompaniets@etu.univ-smb.fr>
2024-03-27 16:57:28 +0100

    rajout d'une fonction à Coloris pour avoir tous les coloris d'un produit

--- FIFA_API/Models/DataManager/ColorisManager.cs
+++ FIFA_API/Models/DataManager/ColorisManager.cs
@@ -2,10 +2,12 @@
 using FIFA_API.Models.Repository;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+
 
 namespace FIFA_API.Models.DataManager
 {
-    public class ColorisManager : IDataRepository<Coloris>
+    public class ColorisManager : IColorisRepository
     {
         private readonly FifaDbContext fifaDbContext;
 
@@ -51,5 +53,32 @@ namespace FIFA_API.Models.DataManager
             entityToUpdate.VariantesColorises = entity.VariantesColorises;
             await fifaDbContext.SaveChangesAsync();
         }
+
+        public async Task<ActionResult<IEnumerable<Coloris>>> GetColorisProduit(int idProduit)
+        {
+            List<VarianteProduit> variantesProduit = await fifaDbContext.VarianteProduit
+                .Where(vp => vp.ProduitId == idProduit)
+                .ToListAsync();
+
+            if (variantesProduit.Count == 0)
+                return null;
+
+            List<Coloris> coloris = new List<Coloris>();
+
+            foreach (var varianteProduit in variantesProduit)
+            {
+                await fifaDbContext.Entry(varianteProduit)
+                                  .Reference(vp => vp.ColorisVariante)
+                                  .LoadAsync();
+
+                if (!coloris.Contains(varianteProduit.ColorisVariante))
+                    coloris.Add(varianteProduit.ColorisVariante);
+            }
+
+            return new ActionResult<IEnumerable<Coloris>>(coloris);
+        }
+
+
+
     }
 }
