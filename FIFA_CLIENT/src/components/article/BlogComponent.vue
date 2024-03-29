0ff3ddec74a65789335f90966940d731c86c15c6
Akinaru <maxime.utchimata@gmail.com>
2024-03-29 08:46:18 +0000

    blog/articles

--- FIFA_CLIENT/src/components/article/BlogComponent.vue
+++ FIFA_CLIENT/src/components/article/BlogComponent.vue
@@ -2,10 +2,19 @@
     <RouterLink :to="{name: 'blog'}">
         <div class="btn btn-block h-44">
             <div class="flex flex-col items-center justify-center">
-                <p class="text-2xl m-3">Kylian Mbappé : Une Légende en Devenir</p>
-                <p class="text-lg">Cet article explore la carrière fulgurante de Kylian Mbappé, sa montée en puissance dans le monde du football et son rôle en tant que source d'inspiration pour la nouvelle génération. </p>
-                <p class="text-xs m-5">Cliquez pour regarder le blog</p>
+                <p class="text-2xl m-3" v-if="blog">{{ blog.blogTitre}}</p>
+                <p class="text-lg" v-if="blog">{{ blog.blogResume }}</p>
+                <p class="text-xs m-5" v-if="blog">Cliquez pour regarder le blog</p>
             </div>
         </div>
+
     </RouterLink>
 </template>
+
+<script setup>
+    import { defineProps } from 'vue';
+    const props = defineProps({
+        blog: Object
+    });
+
+</script>
