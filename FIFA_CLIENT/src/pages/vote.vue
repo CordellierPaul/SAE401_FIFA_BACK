241477f5ba7575e91a855aae019f6c3f9e6773f8
anna.kompaniets <anna.kompaniets@etu.univ-smb.fr>
2024-03-29 09:08:51 +0000

    vote.vue (tj pas fonctionnel)

--- FIFA_CLIENT/src/pages/vote.vue
+++ FIFA_CLIENT/src/pages/vote.vue
@@ -5,7 +5,8 @@
     
     const router = useRouter();
     const route = useRoute();
-
+    const joueurs = ref([]);
+    
     const props = defineProps({
     });
 
@@ -14,7 +15,6 @@
         router.back()
     }
 
-    const joueurs = ref([]);
 
 
     async function fetchJoueurs() {
@@ -34,6 +34,56 @@
     }
 
     onMounted(fetchJoueurs);
+
+    function voter() {
+        const selectedPlayers = [];
+        const selects = document.querySelectorAll('select');
+        
+        for (let i = 0; i < selects.length; i++) {
+            const joueurId = selects[i].value;
+            if (!joueurId) {
+                alert('Veuillez sélectionner un joueur pour chaque option.');
+                return; 
+            }
+
+            if (selectedPlayers.includes(joueurId)) {
+                alert('Veuillez sélectionner des joueurs différents pour chaque option.');
+                return; 
+            }
+
+            selectedPlayers.push(joueurId);
+        }
+
+        //const userId = /* ID de l'utilisateur connecté */;
+        const themeId = route.query.id;
+        const votes = [];
+
+        for (let i = 0; i < selects.length; i++) {
+            const joueurId = selects[i].value;
+            const selectNum = i + 1;
+            votes.push({ userId, themeId, joueurId, selectNum });
+        }
+
+  //   try {
+  //       const response = await fetch('https://apififa.azurewebsites.net/api/Vote', {
+  //           method: 'POST',
+  //           headers: {
+  //               'Content-Type': 'application/json'
+  //           },
+  //           body: JSON.stringify(votes)
+  //       });
+
+  //       if (!response.ok) {
+  //           throw new Error('Erreur lors de la requête.');
+  //       }
+
+  //       // Réussite - faire quelque chose en conséquence
+  //   } catch (error) {
+  //       console.error('Erreur lors de la requête fetch :', error);
+  //       // Gérer l'erreur
+  //   }
+  }
+
 </script>
 
 
@@ -41,17 +91,54 @@
     <a @click= "retour"  class="hover:opacity-50 hover:cursor-pointer">Revenir à Themes</a>
 
     <div>
-      <h1>Liste des joueurs</h1>
-        <br>
-      <ul v-for="joueur in joueurs" :id="joueur.joueurId">
-        <li >
-          {{ joueur.joueurNom, joueur.joueurPrenom }}
-          <button class="btn btn-primary text-white">
-            Voter
-          </button>
-        </li>
-      </ul>
-      <br>
+      <table>
+        <tr>
+          <td>
+            <h1>Liste des joueurs</h1>
+              <br>
+            <ul v-for="joueur in joueurs" :id="joueur.joueurId">
+              <li >
+                {{ joueur.joueurNom, joueur.joueurPrenom }}
+              </li>
+              
+            </ul>
+            <!-- CECI EST UN EXEMPLE VU QUE LE FETCH N4ET PAS FONCTIONNEL POUR LE MOMENT -->
+            <ul>
+              <li >
+                hehe (ceci est un joueur)
+              </li>
+            </ul>
+            <br>
+            <br>
+          </td>
+          <td>
+                    <select name="joueur1" id="joueur1">
+                        <option v-for="joueur in joueurs" :key="joueur.joueurId" :value="joueur.joueurId">
+                            {{ joueur.joueurNom }}, {{ joueur.joueurPrenom }}
+                        </option>
+                    </select>
+                </td>
+                <td>
+                    <select name="joueur2" id="joueur2">
+                        <option v-for="joueur in joueurs" :key="joueur.joueurId" :value="joueur.joueurId">
+                            {{ joueur.joueurNom }}, {{ joueur.joueurPrenom }}
+                        </option>
+                    </select>
+                </td>
+                <td>
+                    <select name="joueur3" id="joueur3">
+                        <option v-for="joueur in joueurs" :key="joueur.joueurId" :value="joueur.joueurId">
+                            {{ joueur.joueurNom }}, {{ joueur.joueurPrenom }}
+                        </option>
+                    </select>
+                </td>
+
+        </tr>
+      </table>
+    
+      <button class="btn btn-primary text-white">
+        Voter pour 
+      </button>
     </div>
 </template>
 
