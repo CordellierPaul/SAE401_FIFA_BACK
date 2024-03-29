1fb33ce0f5a905b275da01d343a08fd7717f6572
Akinaru <maxime.utchimata@gmail.com>
2024-03-29 08:05:48 +0000

    a

---
+++ FIFA_CLIENT/src/components/article/ArticleComponent.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="collapse collapse-arrow bg-base-300">
+        <input type="radio" name="my-accordion-2" /> 
+        <div class="collapse-title text-xl font-medium">
+            <div class="badge badge-outline badge-sm">Article</div>
+            <div class="flex justify-between items-center"> 
+                
+                <div class="font-bold text-2xl selec">Kylian Mbappé : L'Étoile Montante du Football</div>
+                <p class="text-base">Écrit le: 26/03/2024</p>
+            </div>
+            <p class="font-semibold">Découvrez comment Kylian Mbappé, avec son talent inégalé et son charisme, a conquis le monde du football.</p>
+            <div class="divider"></div>
+            <p class="font-normal my-3">Kylian Mbappé est une force inarrêtable sur le terrain. À seulement 23 ans, il a déjà marqué l'histoire du football. Son mélange de vitesse, d'agilité, et de finition en fait un attaquant exceptionnel. Il a remporté la Coupe du Monde 2018 avec l'équipe nationale française et a conquis la Ligue 1 avec le Paris Saint-Germain. Mais ce qui le distingue encore plus, c'est son leadership et son humilité. Mbappé est un modèle pour les jeunes joueurs, incarnant l'esprit d'équipe et la passion pour le jeu. Avec un avenir prometteur devant lui, il est destiné à rester une légende du football.</p>
+        </div>
+        <div class="collapse-content *:my-3"> 
+            <BlogComponent></BlogComponent>
+        </div>
+    </div>
+</template>
+
+<script setup>
+    import { onMounted } from 'vue';
+    import BlogComponent from './BlogComponent.vue'
+
+
+
+
+</script>
